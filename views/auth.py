0044609f3143c0eb615d6b5ebf1800b78dc5ae10
Masyanja <107952755+Masyanja@users.noreply.github.com>
2022-09-24 18:44:51 +0300

    Add files via upload

---
+++ views/auth.py
@@ -0,0 +1,30 @@
+from flask_restx import Resource, Namespace
+from flask import request
+from implemented import user_service
+from service.auth import generate_token, approve_token
+
+auth_ns = Namespace('auth')
+
+
+@auth_ns.route('/')
+class AuthView(Resource):
+    def post(self):
+        data = request.json
+        username = data.get("username")
+        password = data.get("password")
+
+        if username or not password:
+            return "Нет пароля или логина", 400
+
+        user = user_service.get_by_username(username=username)
+        return generate_token(username=username,
+                              password=password,
+                              password_hash=user.passsword,
+                              is_refresh=False), 201
+
+    def put(self):
+        data = request.json
+        if not data.get("refresh_token"):
+            return "", 400
+
+        return approve_token(data.get("refresh_token")), 200
